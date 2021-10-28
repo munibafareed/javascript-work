383a4d0dcce7028b0e29ba2f5e19e6fc73490b9d
munibafareed <fmuniba34@gmail.com>
2021-10-27 21:33:38 -0700

    work complet

---
+++ app.js
@@ -0,0 +1,158 @@
+// CHAPTER#1 ALERT
+
+// alert("HELLO WORLD")
+// alert("welcome to JS land \n happy codding")
+
+// chap#2
+
+// var name = "MUNIBA FAREED" ;
+// alert(name)
+// var number = 25 ;
+// var name = "year old" ;
+// alert(25+"year old")
+// var name2 = "certified of web & mobile developer"
+// alert("pizza \npizz \npiz \npi \np")
+// alert("my email adress is muniba@gamil.com")
+// var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” "
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+// chap#3
+
+// var name1 = "im" ;
+// var num = 25 ;
+// var name2 = "years old" ;
+// alert(name1  + " " + num  +  name2)
+// var name1 = "you have visited this side";
+// var num = 15 ;
+// var name2 = "times";
+// alert(name1 + "" + num + "" +name2)
+
+// chap#4
+
+// document.write("b)  Variable names can only contain number, $, alphabat and _. For example $my_1stVariable <br> c) Variables must begin with a letter, $ or _. For example $name, _name or name <br>S d) Variable names are case sensetive e) Variable names should not be JS keyword ")
+
+// chap#5
+
+// var num1 = 4 ;
+// var num2 = 2 ;
+// alert(num1 + num2)
+// document.write("sum of" + 4 + "and" + 2 + "is" + 6)
+
+// var a = 3 ;
+// var result = ++a
+//             3
+// alert(result)
+
+// var result = 4 + 7;
+// alert (result)
+
+// var a = 11 ;
+// var result = --a;
+//              11
+// alert(result)
+// var result = 10/3;
+// alert(result)
+
+// document.write("initiale value is:" + 3)
+// document.write(" <br> value after increament:" + 4)
+// document.write(" <br> value after addition:" + 7)
+// document.write("<br> value after decreament: " + 10)
+// document.write("<br> the reminder: " + 3.333)
+    
+// chap#6to9
+
+// var a = 2 ;
+// var b = 1 ;
+// var result = --a - --b + ++b + b--;
+//             1 - 0 + 2 + 1
+// alert(result)
+// document.write("a=" + 2)
+// document.write(" <br> b =" + 1)
+// document.write(" <br> result=" +3)
+
+//  var b = 1 ;
+//  var a = 2 ;
+//  var result1 = --a; 
+//               1
+// var result2 = --a - --b;
+//               1 - 0 
+// var result3 = --a - --b + ++b;
+//                1 - 0 + 3 
+//  var result4 = --a - --b + ++b + b--;
+//                 1 - 0 + 3 + 1
+// alert(1)
+// alert(1-0)
+// alert(1-0+3)
+// alert(1-0+3+1)
+
+// chap#9to11
+
+// var karchi =prompt("welcome to city of light")
+// var meetup =prompt("give message female")
+// var warning =prompt("please show ")
+
+// document.write("<table border = 1, width= 500 >")
+// document.write("<tr>")
+
+// document.write("<td>signal color</td>")
+// document.write("<td>message</td>")
+
+// document.write("</tr>")
+
+
+// document.write("<table border = 1, width= 500 >")
+// document.write("<tr>")
+
+// document.write("<td>red</td>")
+// document.write("<td>must stop</td>")
+
+// document.write("</tr>")
+
+// document.write("<table border = 1, width= 500 >")
+// document.write("<tr>")
+
+// document.write("<td>yellow</td>")
+// document.write("<td>ready to move</td>")
+
+// document.write("</tr>")
+
+// document.write("<table border = 1, width= 500 >")
+// document.write("<tr>")
+
+// document.write("<td>green</td>")
+// document.write("<td>move on</td>")
+
+// document.write("</tr>")
+// document.write("</table>")
+
+
+//  var a = 4;
+//  if (++a === 5){
+//       alert("you are eligibale"); }
+
+//  var b = 82; 
+// if (b++ === 83){ 
+//       alert("you are not eligibale"); } 
+//      var c = 12; 
+//      if (c++ === 13){ alert("you are eligibale"); } 
+//      if (c === 13){ alert("you are not eligibale"); }
+//       if (++c < 14){ alert("you are eligibale"); } 
+//       if(c === 14){ alert("you are not eligibale"); } 
+ 
+//      var materialCost = 20000;
+//      var laborCost = 2000;
+//     var totalCost = materialCost + laborCost;
+//     alerta( materialCost + labourCost)
+//      if (totalCost === laborCost + materialCost){ 
+//          alert("The cost equals"); } 
+       
+//      if (true){ alert("True"); }
+//      if (false){ alert("False"); } 
+       
+      
+       
+//      if("car" < "cat")
+//      alert("car is smaller than cat"); 
+
+ 
+ 
